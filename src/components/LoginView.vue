93e9f3f278e8b0ded35d53f900544911e7e5a3ae
thanhquyet96 <thanhquyet9670@gmail.com>
2023-03-06 18:19:17 +0700

    first commit

---
+++ src/components/LoginView.vue
@@ -0,0 +1,60 @@
+﻿<template>
+    <div class="main-wrapper account-wrapper">
+        <div class="account-page">
+            <div class="account-center">
+                <div class="account-box">
+                    <form action="index.html" class="form-signin">
+                        <div class="account-logo">
+                            <a href="index.html"><img src="assets/img/logo-dark.png" alt=""></a>
+                        </div>
+                        <div class="form-group">
+                            <label>Tài khoản</label>
+                            <input type="text" autofocus="" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label>Mật khẩu</label>
+                            <input type="password" class="form-control">
+                        </div>
+                        <div class="form-group text-right">
+                            <a href="forgot-password.html">Quên mật khẩu?</a>
+                        </div>
+                        <div class="form-group text-center">
+                            <button type="submit" class="btn btn-primary account-btn">Đăng nhập</button>
+                        </div>
+                        <div class="text-center register-link">
+                            Bạn chưa có tài khoản? <a href="register.html">Đăng ký</a>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>export default {
+  name: 'LoginView',
+  props: {
+    msg: String
+  }
+}</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  /*  h3 {
+        margin: 40px 0 0;
+    }
+
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+
+    li {
+        display: inline-block;
+        margin: 0 10px;
+    }
+
+    a {
+        color: #42b983;
+    }*/
+</style>
---
+++ src/components/SidebarComponent.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="sidebar" id="sidebar">
+              <div class="sidebar-inner slimscroll">
+                  <div id="sidebar-menu" class="sidebar-menu">
+                      <ul>
+                          <li class="menu-title">Main</li>
+                          <li>
+                              <a href="/"><i class="fa fa-dashboard"></i> <span>Dashboard</span></a>
+                          </li>
+                          <li class="submenu">
+                              <a href="#"><i class="fa fa-user"></i> <span> Quản lý nhân Viên </span> <span class="menu-arrow"></span></a>
+                              <ul style="display: none;">
+                                  <li><a href="employees.html">Danh sách</a></li>
+                                  <li><a href="leaves.html">Phê duyệt</a></li>
+                                  <li><a href="holidays.html">Lịch sử</a></li>
+                                  <li><a href="attendance.html">Chấm công</a></li>
+                                  <li><a href="attendance.html">Nghỉ phép</a></li>
+                              </ul>
+                          </li>
+                          <li class="submenu">
+                              <a href="#"><i class="fa fa-money"></i> <span> Quản lý quyền</span> <span class="menu-arrow"></span></a>
+                              <ul style="display: none;">
+                                  <li><a href="invoices.html">Danh sách</a></li>
+                                  <li><a href="payments.html">Payments</a></li>
+                                  <li><a href="expenses.html">Expenses</a></li>
+                                  <li><a href="taxes.html">Taxes</a></li>
+                                  <li><a href="provident-fund.html">Provident Fund</a></li>
+                              </ul>
+                          </li>
+                      </ul>
+                  </div>
+              </div>
+          </div>
+</template>
